28d166f7722c60f36dc1633ec14d7295efe1c6c0
Halil Zabun <halil.zabun@icloud.com>
2025-10-15 07:31:28 +0200

    fix: wrong variable name in row count utility

--- snippets/basic_count_csv_rows.py
+++ snippets/basic_count_csv_rows.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 trainLabelCSVPath = "data/label/train_labeld.csv"
-trainLabelCSVPath = "data/label/train_labeld.csv"
-df = pd.read_csv(csvFilePath)
+df = pd.read_csv(trainLabelCSVPath)
 
 print(df.shape[0])
