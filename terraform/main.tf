781d7a9bb797a6b3a7eea375d9f6b0fd5526da33
Halil Zabun <halil.zabun@icloud.com>
2025-11-09 07:15:36 +0100

    feat: setup ECR repos for preprocessing and training

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,36 @@ module "vpc" {
   tags = local.tags
 }
 
+resource "aws_ecr_repository" "player_churn_preprocess_repo" {
+  name                 = "player-churn-preprocess"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "dev-*"
+    filter_type = "WILDCARD"
+  }
+}
+
+resource "aws_ecr_repository" "player_churn_train_repo" {
+  name                 = "player-churn-train"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "dev-*"
+    filter_type = "WILDCARD"
+  }
+}
+
 # module "eks" {
 #   source  = "terraform-aws-modules/eks/aws"
 #   version = "21.8.0"
