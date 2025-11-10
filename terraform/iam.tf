e127850c761ac15cc8f73d30f25f20a641ba8e4e
Halil Zabun <halil.zabun@icloud.com>
2025-11-10 20:05:37 +0100

    feat: setup EKS via terraform

---
+++ terraform/iam.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name        = "AmazonEKSClusterRole"
+  description = "Allows access to other AWS service resources that are required to operate clusters managed by EKS."
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,8 @@ module "vpc" {
   name = "main-mlops-vpc"
   cidr = "10.0.0.0/24"
 
-  azs             = ["us-east-1a"]
-  private_subnets = ["10.0.0.0/25"]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.0.0/26", "10.0.0.64/26"]
   public_subnets  = ["10.0.0.128/25"]
 
 
@@ -55,7 +55,31 @@ resource "aws_ecr_repository" "player_churn_train_repo" {
   }
 }
 
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "21.8.0"
-# }
+resource "aws_eks_cluster" "player_churn_prediction" {
+  name    = "player-churn-prediction"
+  version = "1.33"
+
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  bootstrap_self_managed_addons = false
+
+  vpc_config {
+    subnet_ids              = module.vpc.private_subnets
+    endpoint_public_access  = true
+    endpoint_private_access = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+
+  zonal_shift_config {
+    enabled = false
+  }
+
+  # Prevent recreation on certain attribute changes
+  lifecycle {
+    ignore_changes = [
+      access_config[0].bootstrap_cluster_creator_admin_permissions,
+    ]
+  }
+}
