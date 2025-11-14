d81b768125c94f67f01e9293a7894458f9cf1bdb
Halil Zabun <halil.zabun@icloud.com>
2025-11-14 06:59:27 +0100

    fix: add linux platform parameter for fargate compatibility

--- scripts/push-images-to-ecr.sh
+++ scripts/push-images-to-ecr.sh
@@ -4,11 +4,11 @@ source "$(dirname "$0")/ecr_env.sh"
 
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ECR_URL"
 
-docker build -t "$ECR_REPO_PREPROCESS" -f "$DOCKERFILE_PREPROCESS" .
-docker build -t "$ECR_REPO_TRAIN" -f "$DOCKERFILE_TRAIN" .
+docker build --platform linux/amd64 -t "$ECR_REPO_PREPROCESS" -f "$DOCKERFILE_PREPROCESS" .
+# docker build --platform linux/amd64 -t "$ECR_REPO_TRAIN" -f "$DOCKERFILE_TRAIN" .
 
 docker tag "$ECR_REPO_PREPROCESS:latest" "$ECR_URL/$ECR_REPO_PREPROCESS:latest"
-docker tag "$ECR_REPO_TRAIN:latest" "$ECR_URL/$ECR_REPO_TRAIN:latest"
+# docker tag "$ECR_REPO_TRAIN:latest" "$ECR_URL/$ECR_REPO_TRAIN:latest"
 
 docker push "$ECR_URL/$ECR_REPO_PREPROCESS:latest"
-docker push "$ECR_URL/$ECR_REPO_TRAIN:latest"
+# docker push "$ECR_URL/$ECR_REPO_TRAIN:latest"
